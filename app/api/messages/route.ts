7700255436b1d24c1199b1874e9fe4d2159c202d
DongJin Kim <80329234+dongjin2008@users.noreply.github.com>
2023-11-23 05:40:03 +0000

    AJAX added

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,15 +1,18 @@
 import { NextResponse } from "next/server";
 import prisma from "../../../lib/prisma";
+import { io } from "socket.io-client";
 
 export async function GET() {
   const messages = await prisma.message.findMany();
+
   return NextResponse.json(messages);
 }
 
 export async function POST(request: Request) {
   const body = await request.json();
-  const message = await prisma.message.create({ data: body });
-  return NextResponse.json(message);
+  const new_message = await prisma.message.create({ data: body });
+  io().emit("new_message", new_message);
+  return NextResponse.json(new_message);
 }
 
 export async function PUT(request: Request) {
--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -3,42 +3,63 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 interface Message {
+  id: number;
   username: string;
   message: string;
 }
 
 export default function Chat() {
-  const [messages, setMessages] = useState([]);
+  const [data, setData] = useState<Message[]>([]);
+  const [message, setMessage] = useState('');
 
-  useEffect(() => {
-    const getMessages = async () => {
+  const handleSubmit = async (message: string) => {
+    try {
+      const username = localStorage.getItem('username');
+      await axios.post('/api/messages', { username, message });
+      getMessages();
+      setMessage('');
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  const getMessages = async () => {
       try {
         const response = await axios.get("/api/messages");
-        setMessages(response.data);
+        console.log(response);
+        setData(response.data);
       } catch (error) {
         console.error(error);
       }
     };
+
+  useEffect(() => {
+    
     getMessages();
+
   }, []);
+
   
   return (
     <main className="w-screen h-screen flex">
       <div className="flex justify-center flex-col gap-[2rem]">
         <div className="w-[40.5rem] h-[50rem]">
-          {messages.map((message: Message) => {
+          {data && data.map((message: Message) => {
             return (
               <div className="flex justify-start items-center gap-[1rem]">
-                <div className="w-[4rem] h-[4rem] rounded-[50%] bg-[#C4C4C4] flex justify-center items-center">{message.username[0]}</div>
+                <div key={message.id} className="w-[4rem] h-[4rem] rounded-[50%] bg-[#C4C4C4] flex justify-center items-center">{message.username[0]}</div>
                 <div className="flex flex-col justify-center items-start gap-[0.5rem]">
-                  <p className="text-[#000000] text-[2rem]">{message.message}</p>
+                  <p key={message.id} className="text-[#000000] text-[2rem]">{message.message}</p>
                 </div>
               </div>
             );
           }
           )}
         </div>
-        <input type="text" />
+        <input type="text" value={message} onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+            handleSubmit(message);
+          }
+        }} onChange={(e) => {setMessage(e.target.value)}}  />
       </div>
       <div>
 
